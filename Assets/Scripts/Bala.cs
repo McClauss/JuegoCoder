68f21cf750c05f43dc180e24b711c5380e8fe87f
McClauss <cmhc85@gmail.com>
2022-06-22 00:07:37 -0400

    V.0.0.3
    Se agrega disparos con otras teclas para desafio complementario

--- Assets/Scripts/Bala.cs
+++ Assets/Scripts/Bala.cs
@@ -6,6 +6,9 @@ public class Bala : MonoBehaviour
     public GameObject prefabBala;
 
     public Transform posicionTorreta;
+    public Transform posicionTorretaJ;
+    public Transform posicionTorretaK;
+    public Transform posicionTorretaL;
 
     void Start()
     {
@@ -21,6 +24,15 @@ public class Bala : MonoBehaviour
     void Crearproyectil(){
         if(Input.GetKeyDown(KeyCode.Space)){
             Instantiate(prefabBala,posicionTorreta.position,transform.rotation);
+        }
+        if(Input.GetKeyDown(KeyCode.J)){
+            Instantiate(prefabBala,posicionTorretaJ.position,transform.rotation);
+        }
+        if(Input.GetKeyDown(KeyCode.K)){
+            Instantiate(prefabBala,posicionTorretaK.position,transform.rotation);
+        }
+        if(Input.GetKeyDown(KeyCode.L)){
+            Instantiate(prefabBala,posicionTorretaL.position,transform.rotation);
         }
         //Instantiate(prefabBala,posicionTorreta.position,transform.rotation);
     }
